dcc3e2e36df4c179b857033213daa64524c09fba
mi5ha6in <mi5ha6in@gmail.com>
2021-10-20 23:37:59 +0300

    adds findeMax function

---
+++ lectures/linear-search/findeMax.js
@@ -0,0 +1,16 @@
+// Найти наибольшее число в последовательности
+
+const sequence = [1, 2, 3, 1, 2];
+
+function findeMax(sequence) {
+  let answer = sequence[0];
+
+  for (let i = 1; i < sequence.length; i++) {
+    if (sequence[i] > answer) {
+      answer = sequence[i];
+    }
+    return answer;
+  }
+}
+
+console.log(findeMax(sequence));
